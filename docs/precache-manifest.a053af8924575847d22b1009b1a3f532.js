1e2c0581009ed35bc2c6d7e0e1e728f3585557fb
Joseph Chan <lonqer@gmail.com>
2020-10-10 10:59:49 +0800

    Add build for GH Pages

---
+++ docs/precache-manifest.a053af8924575847d22b1009b1a3f532.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cf2d8a8548091ed46f4e10801a63803",
+    "url": "/react-touchpad/index.html"
+  },
+  {
+    "revision": "659d5fa2b73196fe7dd0",
+    "url": "/react-touchpad/static/css/main.9c93c178.chunk.css"
+  },
+  {
+    "revision": "5acacb41b04a71983713",
+    "url": "/react-touchpad/static/js/2.145ad3de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-touchpad/static/js/2.145ad3de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "659d5fa2b73196fe7dd0",
+    "url": "/react-touchpad/static/js/main.79357270.chunk.js"
+  },
+  {
+    "revision": "7da8363a4bb6d3d54465",
+    "url": "/react-touchpad/static/js/runtime-main.0be17a37.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-touchpad/static/media/logo.5d5d9eef.svg"
+  }
+]);
